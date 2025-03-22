d12c024fcaad61318f88168624154b52cc18ada7
robisoncrisbrito <robison.brito@gmail.com>
2025-03-22 14:54:48 -0300

    TipTime completo + Tema + icone + teste Instrumentação

--- app/src/androidTest/java/br/edu/utfpr/tiptime_pos2025/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/br/edu/utfpr/tiptime_pos2025/ExampleInstrumentedTest.kt
@@ -1,12 +1,19 @@
 package br.edu.utfpr.tiptime_pos2025
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import org.hamcrest.Matchers.containsString
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +22,22 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule()
+    val activity = ActivityScenarioRule( MainActivity::class.java )
+
     @Test
     fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("br.edu.utfpr.tiptime_pos2025", appContext.packageName)
+
+        onView( withId( R.id.cost_of_service ) )
+            .perform( typeText( "50" ) )
+            .perform( ViewActions.closeSoftKeyboard() )
+
+        onView( withId( R.id.calculate_button ) )
+            .perform( click() )
+
+        onView( withId( R.id.tip_result ) )
+            .check( matches ( withText( containsString( "10" ) ) ) )
+
     }
 }
